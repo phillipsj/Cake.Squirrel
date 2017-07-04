eedd887ba482729717adb3b0239d6eba364f661e
Jamie Phillips <cphill11@gmail.com>
2017-07-04 18:26:05 -0400

    GH16: Adds suport for SyncReleases.
    Adding runner for SyncReleases and unit tests.

--- src/Cake.Squirrel/SquirrelAliases.cs
+++ src/Cake.Squirrel/SquirrelAliases.cs
@@ -95,5 +95,74 @@ namespace Cake.Squirrel {
             var runner = new SquirrelRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
             runner.Run(nugetPackage, settings, new ProcessSettings { RedirectStandardOutput = redirectStandardOutput, Silent = silent });
         }
+
+        /// <summary>
+        /// Runs SyncReleases using the specified settings.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// #tool "Squirrel.Windows" 
+        /// #addin Cake.Squirrel
+        /// 
+        /// Task("SyncReleases")
+        ///  .Does(() => {
+        ///    var settings = new SyncReleasesSettings {
+        ///        ReleaseDirectory = "pathToDirectory"
+        ///        Url = new Uri("https://someurl.com");
+        ///        Token = "myToken"
+        ///    }; 
+        /// 
+        ///    SyncReleases(settings);
+        /// });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+        public static void SyncReleases(this ICakeContext context, SyncReleasesSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var runner = new SyncReleasesRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run(settings);
+        }
+
+        /// <summary>
+        /// Runs SyncReleases using the specified settings, if output should be redirected, and 
+        /// if it should be silent.
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// #tool "Squirrel.Windows" 
+        /// #addin Cake.Squirrel
+        /// 
+        /// Task("SyncReleases")
+        ///  .Does(() => {
+        ///    var settings = new SyncReleasesSettings {
+        ///        ReleaseDirectory = "pathToDirectory"
+        ///        Url = new Uri("https://someurl.com");
+        ///        Token = "myToken"
+        ///    }; 
+        /// 
+        ///    SyncReleases(settings, true, false);
+        /// });
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        /// <param name="redirectStandardOutput">Sets if the output of an tool is written to the <see cref="P:System.Diagnostics.Process.StandardOutput"/> stream.</param>
+        /// <param name="silent">Sets if the tool output should be suppressed.</param>
+        [CakeMethodAlias]
+        public static void SyncReleases(this ICakeContext context, SyncReleasesSettings settings, bool redirectStandardOutput, bool silent)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            var runner = new SyncReleasesRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run(settings, new ProcessSettings { RedirectStandardOutput = redirectStandardOutput, Silent = silent });
+        }
     }
 }
---
+++ src/Cake.Squirrel/SyncReleasesRunner.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.Squirrel {
+    /// <summary>
+    /// The SyncReleases runner.
+    /// </summary>
+    public class SyncReleasesRunner : Tool<SyncReleasesSettings> {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SyncReleasesRunner"/> class.
+        /// </summary>
+        /// <param name="fileSystem">The file system.</param>
+        /// <param name="environment">The environment.</param>
+        /// <param name="processRunner">The process runner.</param>
+        /// <param name="tools">The tool locator.</param>
+        public SyncReleasesRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner,
+            IToolLocator tools) : base(fileSystem, environment, processRunner, tools) { }
+
+        /// <summary>
+        /// Executes SyncReleases with the specificed parameters.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        public void Run(SyncReleasesSettings settings) {
+            if (settings == null) {
+                throw new ArgumentNullException(nameof(settings));
+            }
+
+            Run(settings, GetArguments(settings));
+        }
+
+        /// <summary>
+        ///  Executes SyncReleases with the specificed parameters.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        /// <param name="processSettings">The process settings.</param>
+        public void Run(SyncReleasesSettings settings, ProcessSettings processSettings) {
+            if (settings == null) {
+                throw new ArgumentNullException(nameof(settings));
+            }
+            if (settings == null) {
+                throw new ArgumentNullException(nameof(processSettings));
+            }
+            Run(settings, GetArguments(settings), processSettings, null);
+        }
+        
+        private ProcessArgumentBuilder GetArguments(SyncReleasesSettings settings) {
+            var builder = new ProcessArgumentBuilder();
+            if (settings.ReleaseDirectory != null) {
+                builder.Append("--releaseDir {0}", settings.ReleaseDirectory.FullPath);
+            }
+            if (!string.IsNullOrWhiteSpace(settings.Url)) {
+                builder.Append("--url {0}", settings.Url);
+            }
+            if (!string.IsNullOrWhiteSpace(settings.Token)) {
+                builder.Append("--token {0}", settings.Token);
+            }
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Gets the name of the tool.
+        /// </summary>
+        /// <returns>The name of the tool.</returns>
+        protected override string GetToolName() {
+            return "SyncReleases";
+        }
+
+        /// <summary>
+        /// Gets the possible names of the tool executable.
+        /// </summary>
+        /// <returns>List of possible executable names.</returns>
+        protected override IEnumerable<string> GetToolExecutableNames() {
+            return new[] {"SyncReleases.exe"};
+        }
+    }
+}
